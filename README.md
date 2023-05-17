cc247b84ff95e3c651f2f3bea6c92dcaeb870418
hscampos <120430715+hscampos@users.noreply.github.com>
2023-05-17 19:57:32 -0300

    Chatbot: adicionando nova funcionalidade e atualizando readme

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Chatbot
-Esta é uma aplicação com Integração ao ChatGPT, onde é possível realizar perguntas que serão respondidas pelo bot, gerando um histórico da sua conversa com o mesmo.
+Esta é uma aplicação com Integração ao ChatGPT, onde é possível realizar perguntas que serão respondidas pelo bot, gerando um histórico da sua conversa com o mesmo. Você tem também a opção de salvar o resultado do conteúdo gerado em um arquivo.doc caso queira salvar suas pesquisas para estudos futuros.
 
 Para iniciar essa aplicação, será necessário utilizar os seguintes comandos:
 
--- chatbot_main.py
+++ chatbot_main.py
@@ -1,10 +1,11 @@
 import openai
 import streamlit as st
-import os
 from streamlit_chat import message as msg
+import docx
+import io
 
-# openai.api_key = os.getenv("API_KEY_CHATBOT")
-openai.api_key = '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'
+with open("api_key.txt", "r") as api_key:
+    openai.api_key = api_key.read()
 
 st.title("Chat Bot com Chat GPT")
 st.write("***")
@@ -14,15 +15,20 @@ if 'conversa' not in st.session_state:
 
 pergunta = st.text_input("O que deseja perguntar ?")
 botaoEnviar = st.button("Perguntar")
+
+# Consulta a OpenAI e trata o retorno da resposta para o usuário.
+
 if botaoEnviar:
     st.session_state.conversa.append({"role": "user", "content": pergunta})
     retornoOpenAi = openai.ChatCompletion.create(
-        model = "gpt-3.5-turbo",
-        messages = st.session_state.conversa
+        model="gpt-3.5-turbo",
+        messages=st.session_state.conversa
     )
     st.session_state.conversa.append(
         {"role": "assistant",
-        "content": retornoOpenAi['choices'][0]['message']['content']})
+         "content": retornoOpenAi['choices'][0]['message']['content']})
+
+# Realiza o tratamento da conversa.
 
 if len(st.session_state.conversa) > 0:
     for i in range(len(st.session_state.conversa)):
@@ -30,3 +36,30 @@ if len(st.session_state.conversa) > 0:
             msg("Você: " + st.session_state.conversa[i]['content'], is_user=True)
         else:
             msg("Resposta IA: " + st.session_state.conversa[i]['content'])
+
+# Permite salvar o conteúdo da 'conversa', através de um botão que aparece apenas quando
+# o bot efetua uma resposta.
+
+if len(st.session_state.conversa) > 0:
+    botaoSalvar = st.button("Salvar Conteúdo")
+    if botaoSalvar:
+        trabalho = io.BytesIO()
+        documento = docx.Document()
+        documento.add_heading('Conteúdo Gerado', level=1)
+
+        # Percorre a lista 'conversa' e adiciona ao documento os itens dela,
+        # onde a pergunta é um item e a resposta outro.
+
+        for i in range(len(st.session_state.conversa)):
+            if i % 2 == 0:
+                documento.add_heading("Pergunta", level=2)
+                documento.add_paragraph(st.session_state.conversa[i]['content'])
+            else:
+                documento.add_heading("Resposta", level=2)
+                documento.add_paragraph(st.session_state.conversa[i]['content'])
+
+        documento.save(trabalho)
+        st.download_button(label="Clique aqui para salvar este conteúdo",
+                           data=trabalho,
+                           file_name="",
+                           mime="application/msword")
